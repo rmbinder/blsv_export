6479ebb45fce27cd4bc3d9334950888660d9d992
rmbinder <r-m-b@gmx.net>
2018-11-26 20:01:53 +0100

    logging entry added, #3

--- blsv_export.php
+++ blsv_export.php
@@ -183,7 +183,7 @@ exit;
  */
 function isUserAuthorized($scriptName)
 {
-	global $gDb, $gCurrentUser, $gMessage, $gL10n;
+	global $gDb, $gCurrentUser, $gMessage, $gL10n, $gLogger;
 	
 	$userIsAuthorized = false;
 	$menId = 0;
@@ -196,6 +196,8 @@ function isUserAuthorized($scriptName)
 	
 	if ( $menuStatement->rowCount() === 0 || $menuStatement->rowCount() > 1)
 	{
+		$gLogger->notice('BlsvExport: Error with menu entry: Found rows: '. $menuStatement->rowCount() );
+		$gLogger->notice('BlsvExport: Error with menu entry: ScriptName: '. $scriptName);
 		$gMessage->show($gL10n->get('PLG_BLSV_EXPORT_MENU_URL_ERROR', array($scriptName)), $gL10n->get('SYS_ERROR'));
 	}
 	else
